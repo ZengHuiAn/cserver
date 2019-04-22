b9698c1ce58cb25fc0ded2de0c1dfc3a3884dbe0
snake <18501604724@163.com>
2019-04-22 17:50:00 +0800

    配置

--- arena/gen.sh
+++ arena/gen.sh
@@ -1,3 +1,3 @@
-mysql -uroot -p123456 -h172.16.3.97 aGameMobileConfig_sgk -B -e 'select gid,score,name from config_random_arena_ai' | sed 's/\t/,/g' > arena_enemy_config.csv
+# mysql -uroot -p123456 -h172.16.3.97 aGameMobileConfig_sgk -B -e 'select gid,score,name from config_random_arena_ai' | sed 's/\t/,/g' > arena_enemy_config.csv
 
-awk -f gen_arena_enemy_config.awk arena_enemy_config.csv
+# awk -f gen_arena_enemy_config.awk arena_enemy_config.csv
